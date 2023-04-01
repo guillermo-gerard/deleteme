04bd77c787c3447b3bdfdff16f3987aad4f8b6f3
guillermo_gerard <guilloag@gmail.com>
2023-03-31 21:46:55 -0300

    blink, primer intento

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 const int PIN_LED = D0;
+
 void setup()
 {
+  pinMode(PIN_LED, OUTPUT);
   // put your setup code here, to run once:
 }
 
 void loop()
 {
+  digitalWrite(PIN_LED, HIGH);
+  delay(500);
+  digitalWrite(PIN_LED, LOW);
+  delay(500);
   // put your main code here, to run repeatedly:
 }
