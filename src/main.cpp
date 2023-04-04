d939161767c44ec6789fede2974561eb8343e852
guillermo_gerard <guilloag@gmail.com>
2023-04-03 21:05:51 -0300

    implementacion con botones de aceleracion

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,34 @@
 #include <Arduino.h>
 const int PIN_LED = D0;
+const int PIN_BUTTON_FAST = D1;
+const int PIN_BUTTON_SLOW = D2;
+
+const unsigned int WAIT_TIME_DELTA = 10;
+
+unsigned int waitTime = 2000;
+unsigned int waitTimeButton = 200;
+unsigned long waitingTimeStart;
+unsigned long waitingTimeButtonStart;
 
 void setup()
 {
-  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_BUTTON_FAST, INPUT);
+  pinMode(PIN_BUTTON_SLOW, INPUT);
+  waitingTimeStart = millis();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  if (millis() - waitingTimeButtonStart >= waitTimeButton && digitalRead(PIN_BUTTON_FAST))
+  {
+    waitTime = waitTime - WAIT_TIME_DELTA;
+    waitingTimeButtonStart = millis();
+  }
+
+  if (millis() - waitingTimeStart >= waitTime)
+  {
+    digitalWrite(PIN_LED, !digitalRead(PIN_LED));
+    waitingTimeStart = millis();
+  }
 }
